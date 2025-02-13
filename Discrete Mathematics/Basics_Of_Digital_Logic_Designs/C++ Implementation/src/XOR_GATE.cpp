724c4c179aeec265852fd1770243f5abddfc31d8
syedshazli <syed.abdu1818@gmail.com>
2025-02-13 15:34:51 -0500

    docs for XOR

--- Discrete Mathematics/Basics_Of_Digital_Logic_Designs/C++ Implementation/src/XOR_GATE.cpp
+++ Discrete Mathematics/Basics_Of_Digital_Logic_Designs/C++ Implementation/src/XOR_GATE.cpp
@@ -2,6 +2,10 @@
 
 int XOR_GATE::is_XOR_GATE(int x, int y)
 {
+    // XOR: For an XOR gate to output 1:
+    //      X can be 1, Y can be 1
+    //      For an XOR GATE to output 0:
+    //      X AND Y are 0, or X and Y are both 1
     if ((x && y)<=1 && (x&&y)>=0)
     {
         if ((x==1 && y == 0) || (x==0&&y==1))
